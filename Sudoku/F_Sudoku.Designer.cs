986a912fa6900ba1ed1acf7bc34e2bcd79b5858e
GaetanMartin <gaetan.martin1993@gmail.com>
2015-12-02 13:46:15 +0100

    Formulaire renommé

--- Sudoku/F_Sudoku.Designer.cs
+++ Sudoku/F_Sudoku.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sudoku
 {
-    partial class Form1
+    partial class F_Sudoku
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // F_Sudoku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "F_Sudoku";
+            this.Text = "Sudoku";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Sudoku/F_Sudoku.cs
+++ Sudoku/F_Sudoku.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Sudoku
 {
-    public partial class Form1 : Form
+    public partial class F_Sudoku : Form
     {
-        public Form1()
+        public F_Sudoku()
         {
             InitializeComponent();
         }
